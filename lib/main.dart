1ceadfdcd9ef63a084b1f43732dab9a21fc34d1c
zubairehman <zr@embrace-it.com>
2019-02-24 16:15:15 +0500

    - added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
+        fontFamily: 'ProductSans',
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
