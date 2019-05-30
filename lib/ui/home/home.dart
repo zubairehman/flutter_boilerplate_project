4fa7483290db79c489d5214306ce206a7dc1c6f8
zubairehman <zr@embrace-it.com>
2019-05-30 15:11:21 +0500

    - refactored imports

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,7 +1,10 @@
+import 'package:boilerplate/data/sharedpref/constants/preferences.dart';
+import 'package:boilerplate/routes.dart';
 import 'package:boilerplate/stores/form_store.dart';
 import 'package:boilerplate/widgets/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -34,11 +37,10 @@ class _HomeScreenState extends State<HomeScreen> {
       actions: <Widget>[
         IconButton(
           onPressed: () {
-//            SharedPreferences.getInstance().then((preference) {
-//              preference.setBool(Preferences.is_logged_in, false);
-//              Navigator.of(context).pushReplacementNamed(Routes.login);
-//            });
-            _store.getPosts();
+            SharedPreferences.getInstance().then((preference) {
+              preference.setBool(Preferences.is_logged_in, false);
+              Navigator.of(context).pushReplacementNamed(Routes.login);
+            });
           },
           icon: Icon(
             Icons.power_settings_new,
@@ -62,28 +64,28 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _buildListView() {
     return ListView.separated(
-                itemCount: _store.postsList.posts.length,
-                separatorBuilder: (context, position) {
-                  return Divider();
-                },
-                itemBuilder: (context, position) {
-                  return ListTile(
-                    leading: Icon(Icons.cloud_circle),
-                    title: Text(
-                      '${_store.postsList.posts[position].title}',
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      softWrap: false,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(
-                      '${_store.postsList.posts[position].body}',
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      softWrap: false,
-                    ),
-                  );
-                },
-              );
+      itemCount: _store.postsList.posts.length,
+      separatorBuilder: (context, position) {
+        return Divider();
+      },
+      itemBuilder: (context, position) {
+        return ListTile(
+          leading: Icon(Icons.cloud_circle),
+          title: Text(
+            '${_store.postsList.posts[position].title}',
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            softWrap: false,
+            style: Theme.of(context).textTheme.title,
+          ),
+          subtitle: Text(
+            '${_store.postsList.posts[position].body}',
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            softWrap: false,
+          ),
+        );
+      },
+    );
   }
 }
