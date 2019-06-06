a842bb00bf0bc2c1545d40a90b02b36bcec315ee
Rody Davis <rody.davis.jr@gmail.com>
2019-06-06 13:34:13 -0400

    setting screen

---
+++ lib/ui/settings/settings.dart
@@ -0,0 +1,11 @@
+import 'package:boilerplate/widgets/index.dart';
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: EmptyAppBar(),
+    );
+  }
+}
