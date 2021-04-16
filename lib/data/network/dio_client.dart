a3a6ff20efe7bc505aad1a719a0daa37b724b225
zubair <zubairehman.work@gmail.com>
2021-04-16 14:18:50 +0500

    - added PUT and DELETE methods for dio_client.dart

--- lib/data/network/dio_client.dart
+++ lib/data/network/dio_client.dart
@@ -57,4 +57,54 @@ class DioClient {
       throw e;
     }
   }
+
+  // Put:-----------------------------------------------------------------------
+  Future<dynamic> put(
+      String uri, {
+        data,
+        Map<String, dynamic>? queryParameters,
+        Options? options,
+        CancelToken? cancelToken,
+        ProgressCallback? onSendProgress,
+        ProgressCallback? onReceiveProgress,
+      }) async {
+    try {
+      final Response response = await _dio.put(
+        uri,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response.data;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  // Delete:--------------------------------------------------------------------
+  Future<dynamic> delete(
+      String uri, {
+        data,
+        Map<String, dynamic>? queryParameters,
+        Options? options,
+        CancelToken? cancelToken,
+        ProgressCallback? onSendProgress,
+        ProgressCallback? onReceiveProgress,
+      }) async {
+    try {
+      final Response response = await _dio.delete(
+        uri,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+      );
+      return response.data;
+    } catch (e) {
+      throw e;
+    }
+  }
 }
