2cbe8edbbbc9a3ac9577f14463a0113338bcee24
subhead <subhead@gmail.com>
2026-06-04 22:20:37 +0200

    fix: align Android application package

--- android/app/src/main/kotlin/com/iotecksolutions/flutterboilerplate/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.iotecksolutions.flutter_boilerplate_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/iotecksolutions/todoapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.iotecksolutions.todoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
